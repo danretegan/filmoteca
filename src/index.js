36fa35d0813604055533d7d3c8309645efea135d
Dan Retegan <danretegan@yahoo.com>
2024-01-01 20:45:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import APIMOVIE from './js/api-movie.js';
+import APISEARCH from './js/api-search.js';
+import APIDETAILS from './js/api-details.js';
+import APIGENRES from './js/api-genres.js';
+
 import HEADER from './js/header.js';
+import SPINER from './js/spinner.js';
+import MARKUP from './js/markup.js';
 import GALLERY from './js/gallery.js';
+import PAGINATION from './js/pagination.js';
+import SCROLLBUTTON from './js/scroll-button.js';
 import MODAL from './js/modal.js';
 import MYLIBRARY from './js/my-library.js';
 import FOOTER from './js/footer.js';
+import DARKTOGGLE from './js/dark-toggle.js';
