9671a95ec3fb10480134582700ea078751579753
Dan Retegan <danretegan@yahoo.com>
2024-01-03 12:28:06 +0200

    index update 2

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import APIDETAILS from './js/api-details.js';
 import APIGENRES from './js/api-genres.js';
 
 import HEADER from './js/header.js';
-// import SPINER from './js/spinner.js';
+import SPINER from './js/spinner.js';
 import MARKUP from './js/markup.js';
 import GALLERY from './js/gallery.js';
 import PAGINATION from './js/pagination.js';
 import PAGINATIONSEARCH from './js/pagination-search.js';
-// import SCROLLBUTTON from './js/scroll-button.js';
+import SCROLLBUTTON from './js/scroll-button.js';
 import MODAL from './js/modal.js';
 import MYLIBRARY from './js/my-library.js';
 import FOOTER from './js/footer.js';
