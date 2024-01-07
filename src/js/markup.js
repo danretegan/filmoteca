616066824083379212925270b9cd7065a693efaa
Dan Retegan <danretegan@yahoo.com>
2024-01-07 14:13:49 +0200

    Revert "header search cards setup"
    This reverts commit af15e1cd85b289c8d290238a9b372aea5bafb942.

--- src/js/markup.js
+++ src/js/markup.js
@@ -8,15 +8,16 @@ export function createMovieCard(movie, genres) {
   movieCard.setAttribute('modal-movie-card-open', 'true');
 
   const movieImage = document.createElement('img');
-  // Verificăm dacă există un poster_path valid înainte de a crea URL-ul imaginii
-  if (movie.poster_path) {
-    const imageUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  // If a poster exists, use the poster image:
+  const imageUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+
+  try {
     movieImage.src = imageUrl;
-  } else {
-    // Dacă nu există poster_path, folosim o imagine implicită
+  } catch (error) {
+    // If no poster exists, use a default image:
     movieImage.src = 'https://i.imgur.com/p3MsT9t.jpg';
     movieImage.alt = 'Image not available';
-    console.info('No poster available');
+    console.error('Failed to load image:', error);
   }
 
   movieImage.alt = movie.title;
